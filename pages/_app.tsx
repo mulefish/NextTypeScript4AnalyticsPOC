bb03dd14ee43f80053bf09559e44676bd0dcd511
mulefish <pmontgomery@lululemon.com>
2023-11-08 12:57:19 -0800

    Simplest possible Next Typescript MWAAnalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
-import '@/styles/global.scss';
+import '../styles/globals.scss';
 import {initializeAnalytics} from "@lululemon/mwa-analytics"; 
-console.log("Initialz!")
-initializeAnalytics("Hello world") 
-
 
 export default function App({ Component, pageProps }:any) {
 
-
+  if (typeof window != "undefined") {
+    initializeAnalytics("Hello world") 
+  } 
   return (
     <div className="base">
       {/* Site navigation and title and viewport stuff */}
